0b93e091008cb1d9a530dc74f34016bf5886f98d
bolbekus <bolbekus@gmail.com>
2023-02-21 14:50:26 +0500

    03

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -1,7 +1,11 @@
 #Create a variable called 'number' and assign it the two-digit number
-
+number = 67
+a = number % 10
+b = number // 10 
 #Find the reverse of the number and assign it to a variable called 'answer'.
-
+answer = a * 10 + b
 #Print the value of the 'answer'
+
+print(answer)
 
 
